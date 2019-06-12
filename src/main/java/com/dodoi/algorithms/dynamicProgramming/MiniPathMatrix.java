fe7d550f8bda0f28589335be00950f5ae9c076a1
liujia@dabanjia.com <liujia@dabanjia.com>
2019-06-12 18:16:25 +0800

    矩阵最短路径和增加空间复杂度O(n)的方法

--- src/main/java/com/dodoi/algorithms/dynamicProgramming/MiniPathMatrix.java
+++ src/main/java/com/dodoi/algorithms/dynamicProgramming/MiniPathMatrix.java
@@ -32,8 +32,8 @@ public class MiniPathMatrix {
         }
 
         //计算其他位置的最小路径
-        for(int i = 1; i<miniPathMatrix.length;i++){
-            for(int j = 1; j<miniPathMatrix[0].length;j++){
+        for(int i = 1; i<row;i++){
+            for(int j = 1; j<col;j++){
                 miniPathMatrix[i][j] = Math.min(miniPathMatrix[i-1][j],miniPathMatrix[i][j-1]) + array[i][j];
             }
         }
@@ -46,16 +46,42 @@ public class MiniPathMatrix {
         return miniPathMatrix[row-1][col-1];
     }
 
+    public static int getMiniPathBaseArray(int[][] array){
+        if( array == null || array.length == 0 || array[0] == null || array[0].length == 0 ) {
+            return 0;
+        }
+
+        int col = array[0].length;
+        int[] minPath = new int[col];
+
+        minPath[0] = array[0][0];
+
+        //最小路径矩阵[0][j]的最小路径，第一行的数据
+        for(int i = 1; i<col;i++){
+            minPath[i] = minPath[i-1] + array[0][i];
+        }
+
+        int row = array.length;
+        for( int i = 1; i < row; i++ ){
+            minPath[0] = minPath[0] + array[i][0];
+            for( int j = 1; j < col; j++ ){
+                minPath[j] = Math.min(minPath[j-1],minPath[j]) + array[i][j];
+            }
+        }
+
+        return minPath[col-1];
+    }
 
     public static void main(String[] args){
         // 给定一个M*N(5*4)的矩阵,从左上角开始，每步只能向下或向右走，
         int[][] array = {
                 {1,4,8,5},
                 {5,4,1,3},
-                {2,1,5,4},
+                {4,9,6,8},
                 {3,5,1,0},
                 {5,3,5,3}};
-        System.out.println("最短路径："+getMiniPathBaseMatrix(array));
+        System.out.println("BaseMatrix 最短路径："+getMiniPathBaseMatrix(array));
+        System.out.println("BaseArray 最短路径："+getMiniPathBaseArray(array));
     }
 
     private static void printFormatArray(int array[][]){
