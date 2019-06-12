cf01d7d45814489e2cda6e8654e14603430aa2c7
liujia@dabanjia.com <liujia@dabanjia.com>
2019-06-12 17:55:13 +0800

    矩阵的最小路径和方法一

--- src/main/java/com/dodoi/algorithms/dynamicProgramming/MiniPathMatrix.java
+++ src/main/java/com/dodoi/algorithms/dynamicProgramming/MiniPathMatrix.java
@@ -5,37 +5,47 @@ package com.dodoi.algorithms.dynamicProgramming;
  */
 public class MiniPathMatrix {
 
-    public static int getMiniPath(int[][] array){
+    /**
+     * 方法一，时间复杂度O(M*N)，空间复杂度O(M*N)
+     * @param array
+     * @return
+     */
+    public static int getMiniPathBaseMatrix(int[][] array){
+        if( array == null || array.length == 0 || array[0] == null || array[0].length == 0 ) {
+            return 0;
+        }
+        int row = array.length;
+        int col = array[0].length;
+
         // 存储每个位置[i][j]的最小路径矩阵
-        int[][] miniPathMatrix = new int[5][4];
+        int[][] miniPathMatrix = new int[row][col];
 
         miniPathMatrix[0][0] = array[0][0];
         //最小路径矩阵[i][0]的最小路径,第一列的数据
-        for(int i = 1; i<miniPathMatrix.length;i++){
+        for(int i = 1; i<row;i++){
             miniPathMatrix[i][0] = miniPathMatrix[i-1][0] + array[i][0];
         }
 
         //最小路径矩阵[0][j]的最小路径，第一行的数据
-        for(int j = 1; j<miniPathMatrix[0].length;j++){
+        for(int j = 1; j<col;j++){
             miniPathMatrix[0][j] = miniPathMatrix[0][j-1] + array[0][j];
         }
 
+        //计算其他位置的最小路径
         for(int i = 1; i<miniPathMatrix.length;i++){
             for(int j = 1; j<miniPathMatrix[0].length;j++){
-                int min = miniPathMatrix[i][j-1]>miniPathMatrix[i-1][j]?miniPathMatrix[i-1][j]:miniPathMatrix[i][j-1];
-                miniPathMatrix[i][j] = min + array[i][j];
+                miniPathMatrix[i][j] = Math.min(miniPathMatrix[i-1][j],miniPathMatrix[i][j-1]) + array[i][j];
             }
         }
-
 
         System.out.println("原始数据：");
         printFormatArray(array);
         System.out.println("最小路径矩阵：");
         printFormatArray(miniPathMatrix);
 
-        return miniPathMatrix[array.length-1][array[0].length-1];
-
+        return miniPathMatrix[row-1][col-1];
     }
+
 
     public static void main(String[] args){
         // 给定一个M*N(5*4)的矩阵,从左上角开始，每步只能向下或向右走，
@@ -45,7 +55,7 @@ public class MiniPathMatrix {
                 {2,1,5,4},
                 {3,5,1,0},
                 {5,3,5,3}};
-        System.out.println("最短路径："+getMiniPath(array));
+        System.out.println("最短路径："+getMiniPathBaseMatrix(array));
     }
 
     private static void printFormatArray(int array[][]){
