b0065d51c3a154e2eade3bd53cd15489c0c3477b
liujia@dabanjia.com <liujia@dabanjia.com>
2019-06-12 16:36:51 +0800

    初始提交空项目

---
+++ src/main/java/com/dodoi/algorithms/DodoiAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.dodoi.algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodoiAlgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodoiAlgorithmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dodoi/algorithms/DodoiAlgorithmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dodoi.algorithms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DodoiAlgorithmsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
